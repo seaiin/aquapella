f08d2918a8c0fcd381bb23363cb0955faa957a4b
seaiin <namefinal@gmail.com>
2018-01-26 22:52:36 +0700

    Hold key to increase amplitude

---
+++ Assets/Scripts/Sender.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sender : MonoBehaviour {
+
+    private int amp;
+
+	// Use this for initialization
+	void Start () {
+        amp = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            amp += 10;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Debug.Log(amp);
+            waveSpawn();
+            amp = 0;
+        }
+	}
+
+    private void waveSpawn()
+    {
+        GameObject wave = ObjectPooling.SharedInstance.GetPooledObject("Wave");
+
+        if (wave != null)
+        {
+            wave.SetActive(true);
+            wave.SendMessage("setAmp", amp);
+
+            for (int i = 0; i < wave.transform.childCount; i++)
+            {
+                wave.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    private int amp;
+    SphereCollider sphereCollider;
+
+	// Use this for initialization
+	void Start () {
+        amp = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sphereCollider.radius += 0.1f;
+	}
+
+    public void setAmp(int receiveAmp)
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        amp = receiveAmp;
+        Debug.Log("Wave Amp : " + amp);
+    }
+}
