50b356f64e4f26c8814d0f5eaff26202a7a722b8
squidpurin <punno519@gmail.com>
2018-01-28 15:04:14 +0700

    Added Cutscene
    + Added intro cutscene

---
+++ Assets/Scripts/IntroVideo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class IntroVideo : MonoBehaviour {
+
+    public VideoPlayer vplyr;
+	// Update is called once per frame
+	void Start () {
+        vplyr.loopPointReached += EndReached;
+	}
+
+    void EndReached(VideoPlayer v)
+    {
+        Application.LoadLevel("Start");
+    }
+}
